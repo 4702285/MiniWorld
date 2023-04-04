cfb4ade04740a8315f0049bb49eb5360a3a59377
zengsq <4702285@qq.com>
2023-04-04 18:41:33 +0800

    添加socket处理类

---
+++ Project/Assets/Script/net/sockconnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace mwt
+{
+    public class sockconnection : connection
+    {
+        private Socket m_sock;
+
+        public sockconnection()
+        {
+
+        }
+
+        public override bool bind(int port)
+        {
+        }
+
+        public override connection accept()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool connect(string ip, int port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool send(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] recv()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
